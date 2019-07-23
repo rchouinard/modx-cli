5532119e3e8f500f2ea420f2b354606738d7b17a
Ryan Chouinard <rchouinard@gmail.com>
2019-07-23 16:27:17 -0400

    Define core application name and version

--- src/ModxCli.php
+++ src/ModxCli.php
@@ -7,5 +7,11 @@ use Symfony\Component\Console\Application;
 
 final class ModxCli extends Application
 {
+    public const APP_NAME = 'modx-cli';
+    public const APP_VERSION = '0.0.0-dev';
 
+    public function __construct()
+    {
+        parent::__construct(self::APP_NAME, self::APP_VERSION);
+    }
 }
